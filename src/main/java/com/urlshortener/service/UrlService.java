599f732b3beee2dbf0b0e0f5ff8d3f14d65603be
Satyam Kumar <Satyamkumar08642@gmail.com>
2026-08-18 18:02:36 +0530

    created the object reference variable in  UrlService.java

--- src/main/java/com/urlshortener/service/UrlService.java
+++ src/main/java/com/urlshortener/service/UrlService.java
@@ -1,9 +1,34 @@
 package com.urlshortener.service;
 
-import  org.springframework.stereotype.Service;
+import com.urlshortener.repository.UrlRepository;
 
-
-@Service
 public class UrlService {
     
+    /*
+    By using the object references (OOPS) we can use the method of different class.
+    eg: UrlRepository repository;
+
+    here:
+    UrlRepository ---> means reference type
+    repository. ----> means reference variable
+
+    then we can actually create the object to use: repository = new UrlRepository();
+
+    also both can be used together :
+    UrlRepository repository = new UrlRepository();
+
+    */
+
+    UrlRepository repository; //it create a variable capable of referring to a UrlRepository object.
+
+    public UrlService(){
+        repository = new UrlRepository();
+
+    }
+
+    public void shortenUrl(String originalUrl){
+        repository.saveUrl();
+    }
+    
+
 }
